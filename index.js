1dde4c5034299b03015c6f71c28e3287c6768612
caner <caner-ahmet.turan@dci-student.org>
2025-01-29 15:57:47 +0100

    update

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-console.log("hallo welt");
+const steuer = 0.07;
+let apfelPreis = 0.5;
+let bananePreis = 0.3;
 
+let apfelGesamtpreis = 3 * apfelPreis + 1 * steuer;
+let bananenGesamtpreis = 5 * bananePreis + 1 * steuer;
 
+let gesamtpreis = apfelGesamtpreis + bananenGesamtpreis
 
+apfelGesamtpreismitMehrwertsteuer:1.57
+bananenGesamtpreismitMehrwertsteuer:1.57
+gesamtpreis:3.14
+console.log(bananenGesamtpreis)
